cbcea3e448d77c95428315ee69730d91d58a2d1d
Joshua Karol <josh@themistechnology.com>
2017-10-21 23:02:38 -0500

    comments can now have children

--- src/app/models/child.ts
+++ src/app/models/child.ts
@@ -1,25 +1,21 @@
 import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
 import { AuthService } from '../../providers/auth-service';
-import {Child} from '../models/child'
-export class Comments {
+
+export class Child {
     username: string;
     emailname: string;
     comment: string;
     date: any;
     time: any;
-    childarray?: any;
-    creating: boolean;
     public af: AngularFire;
 
     constructor(username: string, emailname: string,
-        comment: string, date: any, time: any, creating: boolean, childarray?: any) {
+        comment: string, date: any, time: any) {
         this.username = username;
         this.emailname = emailname;
         this.comment = comment;
         this.date = date;
         this.time = time;
-        this.childarray = childarray;
-        this.creating=creating;
         
     }
 }
--- src/pages/comment/comment.ts
+++ src/pages/comment/comment.ts
@@ -5,6 +5,7 @@ import { AuthService } from '../../providers/auth-service';
 import { Login } from '../login/login';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 import { Comments } from '../../app/models/comment';
+import { Child } from '../../app/models/child';
 
 import * as firebase from 'firebase';
 
@@ -22,12 +23,14 @@ export class Comment {
     text: any;
     listOfComments: any;
     dtstring: any;
+    childtext: any;
     constructor(private menu: MenuController, public navCtrl: NavController, private _auth: AuthService, public af: AngularFire, private navParams: NavParams,) {
        
     }
     ngOnInit(){
         this.bill = this.navParams.get('bill');
         this.link = this.af.database.list(this.navParams.get('link')+ this.bill.$key + "/comments");
+        
         this.user = this.af.database.object('/users/' + this._auth.getEmailName());
         
          this.userSubscription = this.user.subscribe((user_data)=>{
@@ -59,11 +62,43 @@ export class Comment {
         var displayTime = new Date().toLocaleTimeString();
         
 
-        let comment = new Comments(this.username, this._auth.getEmailName(), this.text,displayDate, displayTime);
+        let comment = new Comments(this.username, this._auth.getEmailName(), this.text,displayDate, displayTime, false);
         this.link.push(comment);
         this.text="";
 
 
+
+    }
+
+    openChild(){
+
+    }
+
+    makeChild(comment){
+        comment.creating=true;
+    }
+    closeNew(comment){
+        comment.creating=false;
+        this.childtext="";
+        
+    }
+
+    postChild(comment){
+        console.log(comment)
+        var displayDate = new Date().toLocaleDateString();
+        var displayTime = new Date().toLocaleTimeString();
+        let newchild = new Child(this.username, this._auth.getEmailName(), this.childtext, displayDate, displayTime)
+        if(comment.childarray){
+            comment.childarray.push(newchild);
+
+        }else{
+            comment.childarray=[newchild];
+        }
+        comment.creating=false;
+        this.childtext="";
+        let staticlink = this.af.database.object(this.navParams.get('link')+ this.bill.$key + "/comments/"+comment.$key);
+        
+        staticlink.update({childarray: comment.childarray});
 
     }
 
